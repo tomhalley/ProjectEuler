c3c81f882b46145ab95b5f6b11b40c38a33fbfd4
Tom Halley <tomhalley89@gmail.com>
2012-12-15 03:48:09 +0000

    project 10 using Sieve of Eratosthenes

---
+++ Problem10.py
@@ -0,0 +1,34 @@
+numbers = []
+initial_primes = [2]
+
+def isPrime(n):
+	for prime in initial_primes:
+		if n % prime == 0:
+			return False
+	return True
+
+def erasto(n):
+	for candidate in xrange(3, n, 2):
+		if isPrime(candidate): 
+			if candidate**2 > n:
+				break
+			initial_primes.append(candidate)
+
+	for x in xrange(0, n):
+		numbers.insert(x, True)
+	
+	for p in initial_primes:
+		for x in xrange(p, n, p):
+			if p != x:
+				numbers[x] = False
+	
+	numbers[1] = False
+	primes = []
+
+	for x in xrange(0, n):
+		if numbers[x]:
+			primes.append(x)
+
+	return primes
+
+print sum(erasto(2000000))
